c7d1516e00e82e75bd12c8232a3e5bb725167e0a
Raghd <rsmadhun@stud.ntnu.no>
2024-11-03 12:43:16 +0100

    back with the comments
    Hi, i added back the comments i removed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@
 public class Main {
   public static void main(String[] args) {
     Employee employee = new Employee("Raghd", 15000);
+    //System.out.println(employee.getEmpName());
+    //System.out.println(employee.getEmpSalary());
 
     University university = new University("NTNU", 1);
-
+    //university.getUniCode();
+    //university.getUniName();
 
   }
 }
